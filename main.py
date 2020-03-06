892312c76b935918bbd9edbfb439d851312e1b51
mutasemNidal <mutasem.nidal@gmail.com>
2020-03-06 05:15:11 -0800

    get address automaticlly

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 import time
+import netifaces as ni
 from http.server import HTTPServer
 from server import Server
 import socket
 import fcntl
 import struct
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
-
-HOST_NAME = get_ip_address('eth0')
+eth_interface= 'etho'
+lo_interface='lo'
+try:
+    ni.ifaddresses('eht0')
+    ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+except:
+    ni.ifaddresses('lo')
+    ip = ni.ifaddresses('lo')[ni.AF_INET][0]['addr']
+    
+print(ip ) # should print "192.168.100.37"
+HOST_NAME = ip
 PORT_NUMBER = 8000
-
 if __name__ == '__main__':
     httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
     print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
